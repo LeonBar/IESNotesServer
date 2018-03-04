d64b63a6035b57f08076d3d2b432b4b7269aee96
Leon <leon.barkan@gmail.com>
2018-03-04 19:34:35 +0200

    updates

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ var bodyParser = require('body-parser');
 const fs = require('fs');
 var admin = require('firebase-admin');
 
-//var{Note} = require("./models/note");
-//var{User} = require("./models/user");
-//var {mongoose} = require("./db/mangoos");
+var{Note} = require("./models/note");
+var{User} = require("./models/user");
+var {mongoose} = require("./db/mangoos");
 var serviceAccount = require('./fireBase/firebase-keys.json');
 
 const port = process.env.PORT || 3000;
@@ -34,35 +34,35 @@ app.use((req,res,next) => {
 
 app.use(bodyParser.json());
 
-app.post('/SendNote',(req,res) => {
+app.post('/sendNote',(req,res) => {
     // The topic name can be optionally prefixed with "/topics/".
     var topic = 'IES';
 
-    // console.log(`Saving JobSeekerID: ${req.body.JobSeekerID} note to mongoDB.`);
+    console.log(`Saving JobSeekerID: ${req.body.JobSeekerID} note to mongoDB.`);
 
-    // var note = new Note({
-    //     NoteStatusID: req.body.NoteStatusID,
-    //     AppearanceReason : req.body.AppearanceReason,
-    //     JobSeekerID : req.body.JobSeekerID,
-    //     FirsName: req.body.FirsName,
-    //     LastName: req.body.LastName,
-    //     AppearanceDate: req.body.AppearanceDate,
-    //     PreviousAppearanceDate: req.body.PreviousAppearanceDate,
-    //     NextAppearanceDate: req.body.NextAppearanceDate,
-    //     NextAppearanceShiftID: req.body.NextAppearanceShiftID,
+    var note = new Note({
+        NoteStatusID: req.body.NoteStatusID,
+        AppearanceReason : req.body.AppearanceReason,
+        JobSeekerID : req.body.JobSeekerID,
+        FirsName: req.body.FirsName,
+        LastName: req.body.LastName,
+        AppearanceDate: req.body.AppearanceDate,
+        PreviousAppearanceDate: req.body.PreviousAppearanceDate,
+        NextAppearanceDate: req.body.NextAppearanceDate,
+        NextAppearanceShiftID: req.body.NextAppearanceShiftID,
 
-    //     ClarkFirstName: req.body.ClarkFirstName,
-    //     ClarkLastName: req.body.ClarkLastName,
-    //     ClarkStendNumber: req.body.ClarkStendNumber,
-    //     QueueNumber: req.body.QueueNumber,
-    // });
+        ClarkFirstName: req.body.ClarkFirstName,
+        ClarkLastName: req.body.ClarkLastName,
+        ClarkStendNumber: req.body.ClarkStendNumber,
+        QueueNumber: req.body.QueueNumber,
+    });
 
-    // note.save().then((note) => {
-    //    console.log(`Jobseeker ${note.JobSeekerID} Note was saved. (id: ${note._id})`);
-    // },(e) => {
-    //    console.log(`Warning! Jobseeker ${note.JobSeekerID} Note wasn't saved.)`,e);
-    //    //res.status(400).send(e);
-    // });
+    note.save().then((note) => {
+        res.send(note + '\nSuccessfully Added to database');
+       //console.log(`Jobseeker ${note.JobSeekerID} Note was saved. (id: ${note._id})`);
+    },(e) => {
+       res.status(400).send(e);
+    });
 
     // See documentation on defining a message payload.
     var message = {
@@ -85,21 +85,21 @@ app.post('/SendNote',(req,res) => {
     });
 });
 
-// app.post('/SetUser',(req,res) => {
+app.post('/SetUser',(req,res) => {
 
-//     var user = new User({
-//         JobSeekerIdentityCard: req.body.JobSeekerIdentityCard,
-//         MobileNumber: req.body.MobileNumber,
-//         JobSeekerID:req.body.JobSeekerID
-//     });
+    var user = new User({
+        JobSeekerIdentityCard: req.body.JobSeekerIdentityCard,
+        MobileNumber: req.body.MobileNumber,
+        JobSeekerID:req.body.JobSeekerID
+    });
 
-//     user.save().then((user) => {
-//        console.log(`User ${user.JobSeekerID} was saved. (id: ${note._id})`);
-//     },(e) => {
-//        console.log(`Warning! User ${user.JobSeekerID} wasn't saved.)`,e);
-//        //res.status(400).send(e);
-//     });
-// });
+    user.save().then((user) => {
+       console.log(`User ${user.JobSeekerID} was saved. (id: ${note._id})`);
+    },(e) => {
+       console.log(`Warning! User ${user.JobSeekerID} wasn't saved.)`,e);
+       //res.status(400).send(e);
+    });
+});
 
 app.listen(port, ()=>{
     console.log('IESNotes server is running on port : ', port);
